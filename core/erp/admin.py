ca8d9777c51d13db1c6c3d4474c34525a25e4080
Anterd1 <hectorhugo359@gmail.com>
2020-08-11 14:19:39 -0500

    migraciones x2

--- core/erp/admin.py
+++ core/erp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from core.erp.models import Servicio, Blog,Presentacion
+from core.erp.models import Servicio, Blog
 # Register your models here.
 class ServicioAdmin(admin.ModelAdmin):
     readonly_fields = ('created','updated')
 admin.site.register(Servicio, ServicioAdmin)
 admin.site.register(Blog)
-admin.site.register(Presentacion)
